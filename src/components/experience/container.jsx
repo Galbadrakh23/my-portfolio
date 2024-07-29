b909d1df4e617c7960042172dd3806f3c387adef
galbadrakh23 <galbadrak223@gmail.com>
2024-07-29 13:30:46 +0800

    add changes

--- src/components/experience/container.jsx
+++ src/components/experience/container.jsx
@@ -3,7 +3,7 @@ import ExperienceCards from "./experience-card";
 
 const ContainerExperience = () => {
   return (
-    <section className="px-[80px] py-[96px]  flex flex-col items-center justify-center">
+    <section className="px-[80px] py-[96px]  flex flex-col items-center justify-center dark:bg-red-500">
       <ExperienceButton />
       <ExperienceCards />
     </section>
